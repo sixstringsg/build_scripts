c60f3f0f2cfeafb89e7505acca88db3db612cfce
Jason Stewart <sixstringsg@verizon.net>
2012-07-18 18:11:37 -0500

    Add in a buildscript and IRC notifier for FNV builds
    Credit to KhasMek for the buildscript I kanged/butchered, and
    Smasher816 for the Telnet IRC script

---
+++ Build_FNV/FNV.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# $1 should be lunch combo
+# $2 should be device name
+# select device and prepare varibles
+BUILD_ROOT=~/$1
+cd $BUILD_ROOT
+. build/envsetup.sh
+lunch fnv_$2-userdebug
+
+# create log dir if not already present
+if test ! -d "$ANDROID_PRODUCT_OUT"
+    echo "$ANDROID_PRODUCT_OUT doesn't exist, creating now"
+    then mkdir -p "$ANDROID_PRODUCT_OUT"
+fi
+
+# build
+make -j$(($(grep processor /proc/cpuinfo | wc -l) * 2)) bacon 2>&1 | tee "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log
+
+# clean out of previous zip
+ZIP=$(tail -2 "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log | cut -f3 -d ' ' | cut -f1 -d ' ' | sed -e '/^$/ d')
+
+# finish
+echo "$2 build complete"
+
+scp $ZIP website@androtransfer.com:~/www/androtransfer.com/public_html/sixstringsg/$2
+
+if test -x irc ; then
+     ./irc New build is up for $2 get it at http://xfer.in/?developer=sixstringsg&folder=$2
